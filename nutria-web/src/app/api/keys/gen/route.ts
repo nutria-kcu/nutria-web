3b09e97bf4f452e9e3e547b5b18799ed71e198b7
nickroh <roh517@naver.com>
2025-04-17 02:14:51 -0500

    FIX: module

---
+++ nutria-web/src/app/api/keys/gen/route.ts
@@ -0,0 +1,6 @@
+// route.ts
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  return NextResponse.json({ message: 'Hello' });
+}
---
+++ nutria-web/src/app/api/keys/validate/route.ts
@@ -0,0 +1,7 @@
+// route.ts
+
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  return NextResponse.json({ message: 'Hello' });
+}
